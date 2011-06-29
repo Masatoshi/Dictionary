f56c63732f33b0e330bc75c26cb62ec31cffd1e6
Jan Bannister <jan.bannister@gmail.com>
2011-06-29 18:47:37 +0100

    Initial Commit

---
+++ Dictionary/DictionaryViewController.h
@@ -0,0 +1,15 @@
+//
+//  DictionaryViewController.h
+//  Dictionary
+//
+//  Created by Jan Bannister on 29/06/2011.
+//  Copyright 2011 jan bannister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DictionaryViewController : UIViewController {
+    
+}
+
+@end
---
+++ Dictionary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Dictionary
+//
+//  Created by Jan Bannister on 29/06/2011.
+//  Copyright 2011 jan bannister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
